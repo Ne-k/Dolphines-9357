271ab07a68aa76a5b4a2a3141f8de0c7068e14a6
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-08-22 16:50:07 -0700

    edit

--- components/Sections/Members.section.tsx
+++ components/Sections/Members.section.tsx
@@ -7,43 +7,43 @@ const Skills = () => {
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
         <p className="flex flex-row text-slate-300">
-          <BsArrowRightShort size="30" />
+          <BsArrowRightShort size="20" />
           <span className="text-white">Ethan McKelly</span>
           &nbsp;Everything hardware
         </p>
 
           <p className="flex flex-row text-slate-300">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Jack Denning</span>
               &nbsp;Hardware, scouting, and mascot apparently
           </p>
 
           <p className="flex flex-row text-slate-300">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Joss Kirby</span>
               &nbsp;Engineering notebook/help hardware
           </p>
 
           <p className="flex flex-row text-slate-300">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Hannah Compton</span>
               &nbsp;CAD and hardware
           </p>
 
           <p className="flex flex-row text-slate-300">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Cardin Nguyen</span>
               &nbsp;Software and website
           </p>
 
           <p className="flex flex-row text-slate-300">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Tyler Bretthauer</span>
               &nbsp;Software and website
           </p>
 
           <p className="flex flex-row text-slate-300 items-center border-b-[0.1px] border-white py-1">
-              <BsArrowRightShort size="30" />
+              <BsArrowRightShort size="20" />
               <span className="text-white">Sam Felstad</span>
               &nbsp;Ginger whore.
           </p>
