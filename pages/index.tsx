595148d2af141f4c85450080fe62eb903075ba0d
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-08-22 16:45:57 -0700

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
               background-color: #4162b0;
             }
           `}</style>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <div className="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72">
               <Header/>
               <About/>
